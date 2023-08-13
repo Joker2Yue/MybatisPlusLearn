ef33bea80104fd22787aa91aef6f486409dd539d
JokerYue <Joker_Yue@qq.com>
2023-08-13 17:20:59 +0800

    测试

--- src/test/java/com/joker_yue/MybatisPlusQuickStartApplicationTests.java
+++ src/test/java/com/joker_yue/MybatisPlusQuickStartApplicationTests.java
@@ -1,13 +1,24 @@
 package com.joker_yue;
 
+import com.joker_yue.mapper.UserMapper;
+import com.joker_yue.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
-class MybatisPlusQuickStartApplicationTests {
+class MybatisPlusQuickStatApplicationTests {
+    // 继承了BaseMapper。所有的方法都来自父类，我们可以直接拿来用
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
+        // 查询全部用户
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
     }
 
 }
