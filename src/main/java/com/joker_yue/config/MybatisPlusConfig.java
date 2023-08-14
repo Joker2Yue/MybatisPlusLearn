ea92da4e66457d800cc88b08f9dbd7d6ebec3239
JokerYue <Joker_Yue@qq.com>
2023-08-14 16:23:14 +0800

    配置分页过滤器

--- src/main/java/com/joker_yue/config/MybatisPlusConfig.java
+++ src/main/java/com/joker_yue/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.joker_yue.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,20 +18,28 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration  // 配置类
 public class MybatisPlusConfig {
     // 注册乐观锁插件
-
-
-    /* 旧版
-     @Bean
-    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
-        return new OptimisticLockerInterceptor();
-    }
-     */
-
+        /* 旧版
+         @Bean
+        public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+            return new OptimisticLockerInterceptor();
+        }
+         */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
+    }
+
+    // 配置分页拦截器
+    @Bean
+    public PaginationInnerInterceptor paginationInnerInterceptor(){
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        /*
+        * setOverflow(boolean); 设置请求页面大于最大页数后的操作，true返回首页，false继续请求（默认）
+        * setMaxLimit(Long); 设置单页最大条数，默认500，-1为不限制
+        * */
+        return paginationInnerInterceptor;
     }
 
 }
